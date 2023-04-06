78208bae69dd3ffef2f9ddf3dd78375232277a12
Paolo Galeotti <paologaleotti2002@gmail.com>
2023-04-06 21:25:32 +0200

    feat(clip-maker) changed retrieving algorithm

--- src/clip_maker/content_retriever.py
+++ src/clip_maker/content_retriever.py
@@ -1,9 +1,11 @@
+import itertools
 import random
 from concurrent.futures import ThreadPoolExecutor
 
 import praw
 
 from shared.config import settings
+from shared.logger import log
 
 
 def initialize_wrapper():
@@ -13,36 +15,37 @@ def initialize_wrapper():
     return reddit
 
 
-def retrieve_mapped_posts(reddit, pool, max_duration):
+def retrieve_combined_posts(reddit, pool, max_duration):
+    post_retrieve_limit = settings.reddit.post_retrieve_limit
 
     def retrieve_subreddit_posts(subreddit_name):
-        subreddit = reddit.subreddit(subreddit_name)
-        print("Getting posts from", subreddit_name)
+        log.info(f"Getting posts from r/{subreddit_name}...")
 
-        return [post for post in subreddit.hot(limit=150)
-                if post.is_video
-                and post.media.get('reddit_video')
-                and post.media['reddit_video']['duration'] <= max_duration]
+        subreddit = reddit.subreddit(subreddit_name)
+        posts = [post for post in subreddit.hot(limit=post_retrieve_limit)
+                 if post.is_video
+                 and post.media.get('reddit_video')
+                 and post.media['reddit_video']['duration'] <= max_duration]
+
+        log.info(f"Retrieved {len(posts)} from {subreddit_name}")
+        return posts
 
     with ThreadPoolExecutor() as executor:
-        subreddit_posts = list(executor.map(retrieve_subreddit_posts, pool))
+        subreddit_posts = executor.map(retrieve_subreddit_posts, pool)
 
-    return subreddit_posts
+    log.info("Combining posts...")
+    flatten_posts = list(itertools.chain.from_iterable(subreddit_posts))
+
+    log.info(f"Shuffling {len(flatten_posts)} posts...")
+    random.shuffle(flatten_posts)
+    return flatten_posts
 
 
 def retrieve_video_posts(limit: int, max_duration: int):
     reddit = initialize_wrapper()
     pool = settings.reddit.subreddits
 
-    subreddit_mapped_posts = retrieve_mapped_posts(reddit, pool, max_duration)
-
-    posts = []
-    for _ in range(limit):
-        random_sub = random.choice(subreddit_mapped_posts)
-        post = random.choice(random_sub)
-        print(f"selected post {post.id} from {post.subreddit}")
-
-        posts.append(post)
+    posts = retrieve_combined_posts(reddit, pool, max_duration)
 
     videos = random.sample(posts, k=limit)
     return videos
